7c5a3b0869b77144e8a0c7e31bee284ef1bbfbb6
kat <katharinebrooks@hotmail.co.uk>
2022-04-22 19:16:28 +0100

    express server set up with two routes. Heroku set up

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const cors = require("cors"); //to support secure cross-origin requests and data
 const app = express(); //create a new express object and this is how we interact with the express library
 const port = process.env.PORT || 5000;
 const path = require("path");
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+//set EJS as the templating engine with Express
+app.set("view engine", "ejs");
+
+// GET method route, when user visits the '/' url, making a GET request
+app.get("/", (req, res) => {
+  console.log("in the get /");
+  res.send("in the get /");
+});
+
+// GET method route, when user visits the '/visithistory' url, making a GET request
+app.get("/visithistory", (req, res) => {
+  console.log("in the get /visithistory");
+  res.send("in the get /visithistory/");
+});
+// starts the server and listens on the port
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
